1d5ef0b9797384208fdfd722d2d0e79a15b11c2f
Isac Strömberg <isacstromberg@hotmail.com>
2023-09-08 12:18:14 +0200

    Uploading this practice project to GitHub

---
+++ Databinding/BindingOfData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databinding
+{
+    internal class BindingOfData
+    {
+        public string firstname { get; set; } = "Kalle";
+    }
+}
